e422ef0dfe10872c9eb006038b688eac91f48ef5
Piyush <dheergulwani1@gmail.com>
2021-03-28 15:34:27 +0530

    Completing

--- calculator.py
+++ calculator.py
@@ -5,6 +5,8 @@ import time
 import webbrowser as wb
 import speedtest
 import datetime
+from tkcalendar import Calendar
+
 
 #! Making Functional Buttons
 def click(event):
@@ -436,13 +438,47 @@ def speed_test() :
     spt.configure(bg = 'black')
 
 def age_calc() : 
+    
     date = datetime.datetime.now()
     agecalc = Toplevel(calc)
     agecalc.title('Age Calculator')
     agecalc.geometry('500x300')
-    agecalc_img = PhotoImage(file = '')
+    agecalc_img = PhotoImage(file = 'calendar.png')
     agecalc.iconphoto(False, agecalc_img)
-    agecalc.configure(bg = 'False')
+    
+    class Date :
+        def __init__(self, d, m, y):
+                self.d = d
+                self.m = m
+                self.y = y
+            
+    monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    def countLeapYears(d):
+
+        years = d.y
+
+        if (d.m <= 2):
+            years -= 1
+
+        return int(years / 4) - int(years / 100) + int(years / 400)
+
+    def difference(date1, date2) : 
+        n1 = date1.y * 365 + date1.d
+        
+        for i in range(0, date1.m - 1) :
+            n1  += monthDays[i]
+
+        n1 += countLeapYears(date1)
+
+        n2 = date2.y * 365 + date2.d
+        
+        for i in range(0, date2.m -  1) : 
+            n2 +=  monthDays[i]
+        
+        n2 += countLeapYears(date2)
+
+        return (n2 -n1)
 
 
 #! Reachme 
